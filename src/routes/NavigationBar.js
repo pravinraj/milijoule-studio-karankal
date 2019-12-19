b5de2955464aeb830e180d6d3ce90861afb676df
pravinraj <pravinraj87@gmail.com>
2019-12-20 00:20:29 +0530

    updated header design

--- src/routes/NavigationBar.js
+++ src/routes/NavigationBar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {
+	Container,
+	Row,
+	Col,
 	Navbar,
 	Nav,
 	NavDropdown
@@ -8,9 +11,9 @@ import { NavLink } from 'react-router-dom';
 import brandImage from '../images/home/header/karankal-com.png';
 import filter from '../images/home/header/filter.png';
 import nounProfile from '../images/home/header/noun-profile.png';
-import group from '../images/home/header/group.png';
+import cart from '../images/home/header/cart.png';
 import search from '../images/home/header/search.png';
-import combinedShape from '../images/home/header/combined-shape.png';
+import hamburger from '../images/home/header/hamburger.png';
 import shop from '../images/home/header/shop.png';
 import projects from '../images/home/header/projects.png';
 import blog from '../images/home/header/blog.png';
@@ -20,27 +23,30 @@ import phone from '../images/home/header/phone.png';
 
 function NavigationBar(argument) {
 	return (
-		<Navbar fixed="top" className='studio-header-wrapper' collapseOnSelect expand="lg" bg="light" variant="light">
-			<Navbar.Brand href="#" bsPrefix='navbar brand-image'><img src={brandImage} alt='brandImage'/></Navbar.Brand>
-		  	<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-		  	<Navbar.Collapse id="responsive-navbar-nav">
-		    	<Nav className="ml-auto" >
-			      	<Nav.Link bsPrefix='nav-link route-type-shop'><NavLink exact activeClassName='activeNav' to='/shop'><img src={shop} alt='Shop' /></NavLink></Nav.Link>
-			      	<Nav.Link bsPrefix='nav-link route-type-projects'><NavLink exact activeClassName='activeNav' to='/projects'><img src={projects} alt='Projects' /></NavLink></Nav.Link>
-			      	<Nav.Link bsPrefix='nav-link route-type-blog'><NavLink exact activeClassName='activeNav' to='/blog'><img src={blog} alt='Blog' /></NavLink></Nav.Link>
-			      	<NavDropdown className='contact-us-dropdown' title="Contact Us" id="collasible-nav-dropdown">
-			       		<NavDropdown.Item href="#action/3.1" className='contact-us-dropdown-item'><img className='mail-icon' src={mail} alt='mail-icon' /><span className='contact-detail'>hello@karankal.com</span></NavDropdown.Item>
-			       		<NavDropdown.Divider />
-			       		<NavDropdown.Item href="#action/3.2" className='contact-us-dropdown-item'><img className='phone-icon' src={phone} alt='phone-icon' /><span className='contact-detail'>+917829 475707</span></NavDropdown.Item>				       		
-		      		</NavDropdown>
-		      		<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={search} alt='search' /></Nav.Link>
-			      	<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={filter} alt='filter' /></Nav.Link>
-			      	<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={nounProfile} alt='nounProfile' /></Nav.Link>
-			      	<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={group} alt='group' /></Nav.Link>
-			      	<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={combinedShape} alt='combinedShape' /></Nav.Link>
-		    	</Nav>
-		  	</Navbar.Collapse>
-		</Navbar>
+		<Container>
+			<Navbar fixed="top" className='studio-header-wrapper' collapseOnSelect expand="lg" bg="light" variant="light">
+				<Navbar.Brand href="#" bsPrefix='navbar brand-image'><img src={brandImage} alt='brandImage'/></Navbar.Brand>
+			  	<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			  	<Navbar.Collapse id="responsive-navbar-nav">
+			    	<Nav className="mx-auto" >
+				      	<Nav.Link bsPrefix='nav-link route-type'><NavLink exact activeClassName='activeNav' to='/shop'>Shop</NavLink></Nav.Link>
+				      	<Nav.Link bsPrefix='nav-link route-type'><NavLink exact activeClassName='activeNav' to='/projects'>Projects</NavLink></Nav.Link>
+				      	<Nav.Link bsPrefix='nav-link route-type'><NavLink exact activeClassName='activeNav' to='/blog'>Blog</NavLink></Nav.Link>
+				      	<NavDropdown className='contact-us-dropdown' title="Contact Us" id="collasible-nav-dropdown">
+				       		<NavDropdown.Item href="#action/3.1" className='contact-us-dropdown-item'><img className='mail-icon' src={mail} alt='mail-icon' /><span className='contact-detail'>hello@karankal.com</span></NavDropdown.Item>
+				       		<NavDropdown.Divider />
+				       		<NavDropdown.Item href="#action/3.2" className='contact-us-dropdown-item'><img className='phone-icon' src={phone} alt='phone-icon' /><span className='contact-detail'>+917829 475707</span></NavDropdown.Item>				       		
+			      		</NavDropdown>
+			      	</Nav>
+		      		<Nav className='nav-images-list'>
+			      		<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={search} alt='search' /></Nav.Link>
+				      	<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={nounProfile} alt='nounProfile' /></Nav.Link>
+				      	<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={cart} alt='cart' /></Nav.Link>
+				      	<Nav.Link href="#" bsPrefix='nav-link nav-images'><img src={hamburger} alt='combinedShape' /></Nav.Link>
+				     </Nav>
+			  	</Navbar.Collapse>
+			</Navbar>
+		</Container>
 	);
 }
 
