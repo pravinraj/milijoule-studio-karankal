26f5d0f9891484613bce6277e7da53466fcdb8eb
pravinraj <pravinraj87@gmail.com>
2019-12-22 02:49:12 +0530

    updated blog page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Switch, Route } from "react-router-dom";
 import Shop from '../container/Shop';
 import Projects from '../container/Projects';
@@ -10,12 +10,28 @@ import createBrowserHistory from 'history/createBrowserHistory';
 const history = createBrowserHistory();
 
 function Routes() {
+	const screenWidth = window.screen.width;
+
 	return(
 		<Switch>
 			<Route exact path='/' component={Home} history={history} />
 			<Route exact path='/shop' component={Shop} history={history} />
 			<Route exact path='/projects' component={Projects} history={history} />
-			<Route exact path='/blog' component={Blog} history={history} />
+			<Route exact path='/blog'  render={() => (
+				<iframe
+					id='blog-wrapper'
+	                src="https://studiokarankal.wordpress.com/"
+	                frameBorder={0}
+	                style={{
+	                  width: screenWidth + 'px',
+	                  height: document.documentElement.clientHeight + 'px',
+	                  paddingTop: screenWidth > 782 ? '28px' : '15px'
+	                }}
+	                scrolling={'yes'}
+	                align={'left'}
+	                target="iframe_blog"
+              	/>)}
+			/>
 			<Route exact path='/contactus' component={ContactUs} history={history} />
 		</Switch>
 	);
