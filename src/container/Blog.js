68eb4d0f5c3ba301097d21448855789cefee01ff
pravinraj <pravinraj87@gmail.com>
2019-12-22 03:03:04 +0530

    updated  blog

--- src/container/Blog.js
+++ src/container/Blog.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 function Blog() {
+	const screenWidth = window.screen.width;
 	return (
-		<div>Blog inside</div>
+		<div>
+			<iframe
+				title='studio-blog'
+				id='blog-wrapper'
+                src="https://studiokarankal.wordpress.com/"
+                frameBorder={0}
+               	allowfullscreen="true"
+               	webkitallowfullscreen="true" 
+               	mozallowfullscreen="true" 
+               	sandbox = "allow-scripts"
+                style={{
+                  width: screenWidth + 'px',
+                  height: document.documentElement.clientHeight + 'px',
+                  paddingTop: screenWidth > 782 ? '28px' : '15px'
+                }}
+                scrolling={'yes'}
+                align={'left'}
+                target="iframe_blog"
+          	/>
+        </div>
 	);
 }
 
